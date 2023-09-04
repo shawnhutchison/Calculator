42418d87442e11f3db2fdc0e5a68d380388e60fe
Shawn Hutchison <shutchison42@gmail.com>
2023-09-03 23:44:39 -0700

    Add operate function

--- calculator.js
+++ calculator.js
@@ -1,4 +1,6 @@
-
+let num1 = 0;
+let num2 = 0;
+let operator = '';
 
 function add(num1, num2) {
     return num1 + num2;
@@ -9,9 +11,26 @@ function subtract(num1, num2) {
 }
 
 function divide(num1, num2) {
-    return num1 / num2;
+    if (num2 === 0) {
+        return "Error! Cannot divide by zero.";
+    }
+    return  num1 / num2;
 }
 
 function multiply(num1, num2) {
     return num1 * num2;
+}
+
+function operate(num1, operator, num2) {
+    if (operator === '+') {
+        result = add(num1, num2);
+    } else if (operator === '-') {
+        result = subtract(num1, num2);
+    } else if (operator === 'x' || operator === '*') {
+        result = multiply(num1, num2);
+    } else if (operator === '/') {
+        result = divide(num1, num2);
+    } 
+
+    return result;
 }
