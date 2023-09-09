61997a43a51a4a8ada4127ad3b0542fff5eca034
Shawn Hutchison <shutchison42@gmail.com>
2023-09-09 09:05:19 -0700

    Add algo for basic calculator functionality

--- calculator.js
+++ calculator.js
@@ -1,7 +1,14 @@
 let num1 = 0;
 let num2 = null;
-let operator = '';
+let operator = null;
 let displayValue = '';
+
+const numberButtons = document.querySelectorAll('.numbers');
+const operatorButtons = document.querySelectorAll('.operators');
+const clearButton = document.getElementById('clear');
+const equalsButton = document.getElementById('equals');
+const screen = document.getElementById('screen');
+
 
 
 function add(num1, num2) {
@@ -14,6 +21,7 @@ function subtract(num1, num2) {
 
 function divide(num1, num2) {
     if (num2 === 0) {
+        console.log("Error!")
         return "Error! Cannot divide by zero.";
     }
     return  num1 / num2;
@@ -37,6 +45,54 @@ function operate(num1, operator, num2) {
     return result;
 }
 
-function updateDisplayValue(event) {
 
+
+function setDisplayValue(event) {
+    displayValue = displayValue + event.target.innerText;
+    if (!operator) {
+        num1 = Number(displayValue);
+    } else {
+        num2 = Number(displayValue);
+    }
+    screen.innerText = displayValue;
 }
+
+function setOperator(event) {
+    operator = event.target.innerText;
+    displayValue = '';
+}
+
+numberButtons.forEach(numberButton => {
+    numberButton.addEventListener('click', (event) => {
+            setDisplayValue(event);
+      });
+});
+
+operatorButtons.forEach(operatorButton => {
+    operatorButton.addEventListener('click', (event) => {
+            setOperator(event);
+      });
+});
+
+equalsButton.addEventListener('click', (event) => {
+    if (operator !== null && num2 !== null) {
+        console.log('I fired!');
+        num1 = operate(num1, operator, num2);
+        screen.innerText = num1;
+        displayValue = '';
+        operator = null;
+        num2 = null;
+    }
+});
+
+clearButton.addEventListener('click', (event) => {
+    num1 = 0;
+    num2 = null;
+    operator = null;
+    displayValue = '';
+    screen.innerText = '0';
+});
+
+
+
+
